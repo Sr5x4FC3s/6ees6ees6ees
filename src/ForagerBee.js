3ff74df9e4e2c36da9f53ca1c733dd80dc9a062b
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-01-23 19:18:51 -0800

    finished file

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor() {
+  	super(Grub)
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+
+  forage() {
+  	this.treasureChest.push('treasure');
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,12 @@
 class Grub {
+  constructor() {
+  	this.Grub = {};
+  	this.age = 0;
+  	this.color = 'pink';
+  	this.food = 'jelly';
+  }
+
+  eat() {
+  	return 'Mmmmmmmmm jelly';
+  }
 };
